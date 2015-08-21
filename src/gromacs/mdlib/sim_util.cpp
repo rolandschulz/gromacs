eblen/Gromacs-Offload

Fix time accounting for offload: 1) Make separate table for offload operations 2) Fix thread count for CPU 3) Remove Nonbonded force accounting from CPU 4) Add field for waiting for MIC to CPU

src/gromacs/mdlib/sim_util.cpp
@@ -486,7 +486,7 @@ static void do_nb_verlet(t_forcerec *fr,
 
     bCUDA = (nbvg->kernel_type == nbnxnk8x8x8_CUDA);
 
-    if (!bCUDA)
+    if (!bCUDA && !bUseOffloadedKernel)
     {
         wallcycle_sub_start(wcycle, ewcsNONBONDED);
     }
@@ -523,7 +523,7 @@ static void do_nb_verlet(t_forcerec *fr,
         	{
         		sim_ct = create_code_timer();
         		reset_timer(sim_ct);
-        		nbnxn_kernel_simd_2xnn_offload(fr, ic, enerd, flags, ilocality, clearF, nrnb, wcycle);
+        		nbnxn_kernel_simd_2xnn_offload(fr, ic, enerd, flags, ilocality, clearF, nrnb);
         		dprintf(2, "Offload call overhead %f\n", get_elapsed_time(sim_ct));
         		reset_timer(sim_ct);
         	}
@@ -565,7 +565,7 @@ static void do_nb_verlet(t_forcerec *fr,
             gmx_incons("Invalid nonbonded kernel type passed!");
 
     }
-    if (!bCUDA)
+    if (!bCUDA && !bUseOffloadedKernel)
     {
         wallcycle_sub_stop(wcycle, ewcsNONBONDED);
     }
@@ -1436,15 +1436,19 @@ void do_force_cutsVERLET(FILE *fplog, t_commrec *cr,
     	code_timer *ct = create_code_timer();
     	reset_timer(ct);
     	dprintf(2, "Other force time %f\n", get_elapsed_time(sim_ct));
+    	wallcycle_start(wcycle, ewcWAIT_MIC);
     	wait_for_offload();
+    	wallcycle_stop(wcycle, ewcWAIT_MIC);
     	dprintf(2, "Offload wait time %f\n", get_elapsed_time(ct));
     	int j;
     	dprintf(2, "Offload timings:");
     	for (j=0; j<NUM_TIMES; j++)
     	{
-    		dprintf(2, " %f", phi_times[j]);
+    		// dprintf(2, " %f", phi_times[j]);
     	}
     	dprintf(2, "\n");
+        wallcycle_start(wcycle, ewcNB_XF_BUF_OPS);
+        wallcycle_sub_start(wcycle, ewcsNB_F_BUF_OPS);
         nbnxn_atomdata_add_nbat_f_to_f_final(fr->nbv->nbs, eatAll,
                                              fr->nbv->grp[eintLocal].nbat, f,
                                              gmx_omp_nthreads_get(emntDefault));
@@ -1452,6 +1456,8 @@ void do_force_cutsVERLET(FILE *fplog, t_commrec *cr,
         {
         	((real *)fr->fshift)[j] += fr->nbv->grp[eintLocal].nbat->out[0].fshift[j];
         }
+        wallcycle_sub_stop(wcycle, ewcsNB_F_BUF_OPS);
+        wallcycle_stop(wcycle, ewcNB_XF_BUF_OPS);
     }
 
     if (DOMAINDECOMP(cr))
@@ -2674,6 +2680,21 @@ void finish_run(FILE *fplog, t_commrec *cr,
     {
         wallclock_gpu_t* gputimes = use_GPU(nbv) ?
             nbnxn_cuda_get_timings(nbv->cu_nbv) : NULL;
+        // TODO: Should use bOffUseOffloadedKernel, but it isn't in scope...
+#ifdef GMX_OFFLOAD
+        int nthreads = gmx_omp_nthreads_get(emntNonbonded);
+        gmx_wallcycle_t wcycle_offload = wallcycle_init(fplog, wcycle_get_reset_counters(wcycle),
+        		cr, nthreads, nthreads);
+        gmx_cycles_t force_cycles = get_force_cycles_for_offload();
+        gmx_cycles_t reduce_cycles = get_reduce_cycles_for_offload();
+        gmx_cycles_t other_cycles = get_other_cycles_for_offload();
+        wallcycle_add(wcycle_offload, ewcFORCE, force_cycles, inputrec->nsteps);
+        wallcycle_add(wcycle_offload, ewcNB_XF_BUF_OPS, reduce_cycles, inputrec->nsteps);
+        wallcycle_sub_add(wcycle_offload, ewcsNB_F_BUF_OPS, reduce_cycles, inputrec->nsteps);
+        wallcycle_add(wcycle_offload, ewcRUN, force_cycles + reduce_cycles + other_cycles, inputrec->nsteps);
+        wallcycle_sum(cr, wcycle_offload);
+        wallcycle_print(fplog, 1, 0, get_walltime_for_offload(), wcycle_offload, NULL);
+#endif
         wallcycle_print(fplog, cr->nnodes, cr->npmenodes,
                         elapsed_time_over_all_ranks,
                         wcycle, gputimes);

rolandschulz/gromacs

src/gromacs/mdlib/sim_util.cpp
@@ -487,7 +487,7 @@ static void do_nb_verlet(t_forcerec *fr,
 
     bUsingGpuKernels = (nbvg->kernel_type == nbnxnk8x8x8_GPU);
 
-    if (!bUsingGpuKernels)
+    if (!bUsingGpuKernels && !bUseOffloadedKernel)
     {
         wallcycle_sub_start(wcycle, ewcsNONBONDED);
     }
@@ -524,7 +524,7 @@ static void do_nb_verlet(t_forcerec *fr,
         	{
         		sim_ct = create_code_timer();
         		reset_timer(sim_ct);
-        		nbnxn_kernel_simd_2xnn_offload(fr, ic, enerd, flags, ilocality, clearF, nrnb, wcycle);
+        		nbnxn_kernel_simd_2xnn_offload(fr, ic, enerd, flags, ilocality, clearF, nrnb);
         		dprintf(2, "Offload call overhead %f\n", get_elapsed_time(sim_ct));
         		reset_timer(sim_ct);
         	}
@@ -566,7 +566,7 @@ static void do_nb_verlet(t_forcerec *fr,
             gmx_incons("Invalid nonbonded kernel type passed!");
 
     }
-    if (!bUsingGpuKernels)
+    if (!bUsingGpuKernels && !bUseOffloadedKernel)
     {
         wallcycle_sub_stop(wcycle, ewcsNONBONDED);
     }
@@ -1448,15 +1448,19 @@ void do_force_cutsVERLET(FILE *fplog, t_commrec *cr,
     	code_timer *ct = create_code_timer();
     	reset_timer(ct);
     	dprintf(2, "Other force time %f\n", get_elapsed_time(sim_ct));
+    	wallcycle_start(wcycle, ewcWAIT_MIC);
     	wait_for_offload();
+    	wallcycle_stop(wcycle, ewcWAIT_MIC);
     	dprintf(2, "Offload wait time %f\n", get_elapsed_time(ct));
     	int j;
     	dprintf(2, "Offload timings:");
     	for (j=0; j<NUM_TIMES; j++)
     	{
-    		dprintf(2, " %f", phi_times[j]);
+    		// dprintf(2, " %f", phi_times[j]);
     	}
     	dprintf(2, "\n");
+        wallcycle_start(wcycle, ewcNB_XF_BUF_OPS);
+        wallcycle_sub_start(wcycle, ewcsNB_F_BUF_OPS);
         nbnxn_atomdata_add_nbat_f_to_f_final(fr->nbv->nbs, eatAll,
                                              fr->nbv->grp[eintLocal].nbat, f,
                                              gmx_omp_nthreads_get(emntDefault));
@@ -1464,6 +1468,8 @@ void do_force_cutsVERLET(FILE *fplog, t_commrec *cr,
         {
         	((real *)fr->fshift)[j] += fr->nbv->grp[eintLocal].nbat->out[0].fshift[j];
         }
+        wallcycle_sub_stop(wcycle, ewcsNB_F_BUF_OPS);
+        wallcycle_stop(wcycle, ewcNB_XF_BUF_OPS);
     }
 
     if (DOMAINDECOMP(cr))
@@ -2686,6 +2692,21 @@ void finish_run(FILE *fplog, t_commrec *cr,
     {
         struct gmx_wallclock_gpu_t* gputimes = use_GPU(nbv) ? nbnxn_gpu_get_timings(nbv->gpu_nbv) : NULL;
 
+        // TODO: Should use bOffUseOffloadedKernel, but it isn't in scope...
+#ifdef GMX_OFFLOAD
+        int nthreads = gmx_omp_nthreads_get(emntNonbonded);
+        gmx_wallcycle_t wcycle_offload = wallcycle_init(fplog, wcycle_get_reset_counters(wcycle),
+        		cr, nthreads, nthreads);
+        gmx_cycles_t force_cycles = get_force_cycles_for_offload();
+        gmx_cycles_t reduce_cycles = get_reduce_cycles_for_offload();
+        gmx_cycles_t other_cycles = get_other_cycles_for_offload();
+        wallcycle_add(wcycle_offload, ewcFORCE, force_cycles, inputrec->nsteps);
+        wallcycle_add(wcycle_offload, ewcNB_XF_BUF_OPS, reduce_cycles, inputrec->nsteps);
+        wallcycle_sub_add(wcycle_offload, ewcsNB_F_BUF_OPS, reduce_cycles, inputrec->nsteps);
+        wallcycle_add(wcycle_offload, ewcRUN, force_cycles + reduce_cycles + other_cycles, inputrec->nsteps);
+        wallcycle_sum(cr, wcycle_offload);
+        wallcycle_print(fplog, 1, 0, get_walltime_for_offload(), wcycle_offload, NULL);
+#endif
         wallcycle_print(fplog, cr->nnodes, cr->npmenodes,
                         elapsed_time_over_all_ranks,
                         wcycle, gputimes);