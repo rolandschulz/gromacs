bb058bd302d6691567fbc9b45e598a24742615bd
John Eblen <jeblen@acm.org>
2015-09-02 16:16:36 -0400

    Remove unneeded headers

--- src/gromacs/gmxlib/gmx_omp_nthreads.cpp
+++ src/gromacs/gmxlib/gmx_omp_nthreads.cpp
@@ -42,10 +42,6 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include "gromacs/legacyheaders/copyrite.h"
 #include "gromacs/legacyheaders/macros.h"
